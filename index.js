24aa4dd297e0adb534a1e1575af18e49e5f5851a
saisandeepd7 <102012479+saisandeepd7@users.noreply.github.com>
2022-05-14 14:54:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+setTimeout(()=>{
+
+    var h1 = document.querySelector("h1");
+    h1.innerText ="10";
+     console.log(h1.innerText);   
+
+     setTimeout(()=>{
+        var h1 = document.querySelector("h1");
+        h1.innerText ="9";
+         console.log(h1.innerText);
+         setTimeout(()=>{
+            var h1 = document.querySelector("h1");
+            h1.innerText ="8";
+             console.log(h1.innerText);
+             setTimeout(()=>{
+                var h1 = document.querySelector("h1");
+                h1.innerText ="7";
+                 console.log(h1.innerText);
+                 setTimeout(()=>{
+                    var h1 = document.querySelector("h1");
+                    h1.innerText ="6";
+                     console.log(h1.innerText);
+                     setTimeout(()=>{
+                        var h1 = document.querySelector("h1");
+                        h1.innerText ="5";
+                         console.log(h1.innerText); 
+                         setTimeout(()=>{
+                            var h1 = document.querySelector("h1");
+                            h1.innerText ="4";
+                             console.log(h1.innerText); 
+                             setTimeout(()=>{
+                                var h1 = document.querySelector("h1");
+                                h1.innerText ="3";
+                                 console.log(h1.innerText);
+                                 setTimeout(()=>{
+                                    var h1 = document.querySelector("h1");
+                                    h1.innerText ="2";
+                                     console.log(h1.innerText); 
+                                     setTimeout(()=>{
+                                        var h1 = document.querySelector("h1");
+                                        h1.innerText ="1";
+                                         console.log(h1.innerText);
+                                         setTimeout(()=>{
+                                            var h1 = document.querySelector("h1");
+                                            h1.innerText ="Happy Independence Day";
+                                             console.log(h1.innerText); 
+                                             
+                                             
+                                        
+                                        },1000); 
+                                         
+                                         
+                                    
+                                    },1000);                                          
+                                
+                                },1000);                                         
+                            
+                            },1000);                                          
+                        
+                        },1000);
+                                                                  
+                    
+                    },1000);                                           
+                
+                },1000);   
+            
+            },1000);   
+        
+        },1000);  
+    
+    },1000);
+
+
+},1000);
